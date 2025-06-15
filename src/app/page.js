ebbca16e6dfc34a5a5c3e3fda766a62906f4b038
BrunoS06 <brunosantana0696@gmail.com>
2025-06-15 18:11:54 -0300

    Identidade visual e Inicio

--- src/app/page.js
+++ src/app/page.js
@@ -5,28 +5,25 @@ export default function Home() {
       <div id='cabecalho'>
       <img src="/logo.png" alt="Logo" id='logo'/>
         <div id='secoes'> 
-        <p>Inicio</p>
-        <p>Tópicos</p>
-        <p>Perguntas Frequentes</p>
-        <p>Sobre Mim</p>
-        <p>Contato</p>
+        <a>Início</a>
+        <a>Tópicos</a>
+        <a>Perguntas</a>
+        <a>Sobre</a>
+        <a>Contato</a>
+        
         </div>
+        <button><img src="/wpp.png" id="wpp"/> <p>Agendar Sessão Online</p></button>
       </div>
-      <h2 id="iniciotitulo">Inicio</h2>
-      <div id="inicio">
-      
-      <p>Olá, tudo bem?
-Meu nome é Juliana Mendes, sou psicóloga clínica, e meu propósito é te ajudar a entender melhor seus sentimentos, enfrentar os desafios da vida e construir uma jornada mais leve e consciente.
-
-Acredito que cada pessoa carrega sua própria história, com dores, alegrias, medos e conquistas. Meu papel é caminhar ao seu lado, oferecendo acolhimento, escuta ativa e um espaço seguro, livre de julgamentos.
-
-Trabalho com abordagem baseada no autoconhecimento, no fortalecimento emocional e no desenvolvimento pessoal, ajudando você a lidar com ansiedade, estresse, autoestima, relacionamentos e outros aspectos que impactam sua vida.
-
-Se você sente que precisa de apoio para se reconectar consigo mesmo(a) e construir mudanças significativas, saiba que você não está sozinho(a). Estarei aqui para te acompanhar nesse processo.
-
-Seja muito bem-vindo(a)!
-
-💗 Juliana Mendes – Psicóloga Clínica</p>
+      <div id="iniciodiv">
+      <div id="iniciotxttudo">
+        <p id="iniciotxt1">
+        Transformações começam<br></br> com autocuidado.</p>
+        <p id='iniciotxt2'>Neste espaço terapêutico, utilizo a Terapia Cognitivo-Comportamental para ajudar você a compreender seus pensamentos, emoções e comportamentos, promovendo mudanças reais e duradouras. Acolhimento, escuta e direção para o seu bem-estar.</p>
+        <button><img src="/wpp.png" id="wpp"/> <p>Agendar Sessão Online</p></button>
+      </div>
+      <div id="foto">
+      <img src="/foto.jpeg" id="fotojpg"/>
+      </div>
       </div>
 
     </div>
