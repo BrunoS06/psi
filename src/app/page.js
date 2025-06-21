ad00d9e138ad2b26edcf4739e559daed24da800f
BrunoS06 <brunosantana0696@gmail.com>
2025-06-21 19:30:38 -0300

    Responsivo

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 
 export default function Home() {
   return (
-    <div>
+    <div id="pagina">
       <div id='cabecalho'>
       <img src="/logo.png" alt="Logo" id='logo'/>
         <div id='secoes'> 
@@ -14,16 +14,17 @@ export default function Home() {
         </div>
         <button><img src="/wpp.png" id="wpp"/> <p>Agendar Sessão Online</p></button>
       </div>
+      
       <div id="iniciodiv">
-      <div id="iniciotxttudo">
-        <p id="iniciotxt1">
-        Transformações começam<br></br> com autocuidado.</p>
-        <p id='iniciotxt2'>Neste espaço terapêutico, utilizo a Terapia Cognitivo-Comportamental para ajudar você a compreender seus pensamentos, emoções e comportamentos, promovendo mudanças reais e duradouras. Acolhimento, escuta e direção para o seu bem-estar.</p>
-        <button><img src="/wpp.png" id="wpp"/> <p>Agendar Sessão Online</p></button>
-      </div>
-      <div id="foto">
-      <img src="/foto.jpeg" id="fotojpg"/>
-      </div>
+        <div id="iniciotxttudo">
+          <p id="iniciotxt1">
+          Transformações começam<br></br> com autocuidado.</p>
+          <p id='iniciotxt2'>Neste espaço terapêutico, utilizo a Terapia Cognitivo-Comportamental para ajudar você a compreender seus pensamentos, emoções e comportamentos, promovendo mudanças reais e duradouras. Acolhimento, escuta e direção para o seu bem-estar.</p>
+          <button><img src="/wpp.png" id="wpp"/> <p>Agendar Sessão Online</p></button>
+        </div>
+        <div id="foto">
+        <img src="/foto.jpeg" id="fotojpg"/>
+        </div>
       </div>
 
     </div>
